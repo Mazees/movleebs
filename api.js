14b4a98dcdc95c7345b10019658edb43e4db2aa4
Mazees <madaadha21@gmail.com>
2025-11-12 12:04:49 +0700

    fix: search

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const api =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYzU0YjI3NDIzNmU5MzlhOWI4YzIyZTk2NTFlMmJiMyIsIm5iZiI6MTc2MjM5MDU1Mi4yNzAwMDAyLCJzdWIiOiI2OTBiZjIxOGZkMDdhYmI1YWE2MTNjZmYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.8i3MH2Crs-5CNnb_0TTzsIjty36YRXQPjwWZB3FBJgw";
+const baseURL = "https://api.themoviedb.org/3";
 const options = {
   method: "GET",
   headers: {
@@ -11,16 +12,28 @@ const options = {
 async function fetchMovies(page) {
   let result;
   if (page == 0) return [];
-  const url = `https://api.themoviedb.org/3/discover/movie?language=en-US&page=${page}`;
+  const url = `${baseURL}/discover/movie?language=en-US&page=${page}`;
   try {
     const response = await fetch(url, options);
     result = await response.json();
   } catch (error) {
-    result = []
+    result = [];
     console.error(error);
   }
   const prevPages = await fetchMovies(page - 1);
   return [...prevPages, ...result.results];
+}
+
+async function fetchGenres() {
+  const url = `${baseURL}/genre/movie/list?language=en`;
+  try {
+    const response = await fetch(url, options);
+    result = await response.json();
+  } catch (error) {
+    result = [];
+    console.error(error);
+  }
+  return result;
 }
 
 function sortByName(movies) {
@@ -42,20 +55,18 @@ function sortByDate(movies) {
 
 function searchMovies(movies, target) {
   if (target.length <= 0) return movies;
-  sortByName(movies);
+  let mov = sortByName([...movies]);
 
   const keyword = target.toLowerCase();
   const hasil = [];
 
-  const n = movies.length;
+  const n = mov.length;
   const step = Math.floor(Math.sqrt(n));
   let start = 0;
   let end = step;
 
   while (end < n) {
-    const namaAkhir = (
-      movies[end - 1].title
-    ).toLowerCase();
+    const namaAkhir = mov[end - 1].title.toLowerCase();
 
     if (namaAkhir >= keyword) break;
 
@@ -66,10 +77,10 @@ function searchMovies(movies, target) {
   if (end > n) end = n;
 
   for (let i = start; i < end; i++) {
-    const nama = (movies[i].title).toLowerCase();
+    const nama = mov[i].title.toLowerCase();
 
     if (nama.includes(keyword)) {
-      hasil[hasil.length] = movies[i];
+      hasil[hasil.length] = mov[i];
     }
   }
 
